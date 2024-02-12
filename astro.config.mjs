b6c095ec91824d7c4541f673b87e40f87360aca6
Exequiel Rodriguez <exe922@gmail.com>
2024-02-11 23:13:11 -0300

    Home finished and responsive

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,4 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-  scripts: {
-    // Asegúrate de que esté habilitada la ejecución de scripts JS
-    // Puedes agregar más configuraciones según sea necesario
-    allowNodeModules: true,
-  },
-});
+export default defineConfig({});
--- src/scripts/flip.js
+++
@@ -1,7 +0,0 @@
-function flipContainer() {
-  const textContainer = document.querySelector(".text_container");
-  const imgContainer = document.querySelector(".birthday");
-
-  textContainer.classList.add("flipped");
-  imgContainer.classList.remove("hidden");
-}
